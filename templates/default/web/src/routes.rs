5adb533148d2ad6705789863693233500f0af0ea
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-12-08 12:35:35 +0100

    fix formatting

--- templates/default/web/src/routes.rs
+++ templates/default/web/src/routes.rs
@@ -2,6 +2,5 @@ use crate::state::AppState;
 use axum::Router;
 
 pub fn routes(app_state: AppState) -> Router {
-    Router::new()
-        .with_state(app_state)
+    Router::new().with_state(app_state)
 }
