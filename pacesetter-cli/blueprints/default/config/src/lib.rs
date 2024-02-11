d9cdf34d2239a323b60fde9c66f7e260b219a6d3
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-02-11 19:14:00 +0100

    remove default attribute for ServerConfig

--- pacesetter-cli/blueprints/default/config/src/lib.rs
+++ pacesetter-cli/blueprints/default/config/src/lib.rs
@@ -3,7 +3,6 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct Config {
-    #[serde(default)]
     pub server: ServerConfig,
     pub database: DatabaseConfig,
     // add your config settings here…
--- pacesetter-cli/blueprints/minimal/config/src/lib.rs
+++ pacesetter-cli/blueprints/minimal/config/src/lib.rs
@@ -3,7 +3,6 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct Config {
-    #[serde(default)]
     pub server: ServerConfig,
     // add your config settings here…
 }
