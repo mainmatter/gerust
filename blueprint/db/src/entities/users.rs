72e2eac53093d9f5885b9d8868b153b0b773d699
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-05-08 19:44:40 +0200

    WIP: docs for db crate

--- blueprint/db/src/entities/users.rs
+++ blueprint/db/src/entities/users.rs
@@ -2,12 +2,18 @@ use serde::Serialize;
 use sqlx::Postgres;
 use uuid::Uuid;
 
+/// A user record.
 #[derive(Serialize, Debug, Clone)]
 pub struct User {
+    /// The id of the record.
     pub id: Uuid,
+    /// The user's name.
     pub name: String,
 }
 
+/// Loads a user based on the passed token.
+///
+/// If no user exists for the token, [`Option::None`] is returned, otherwise [`Option::Some(User)`] is returned.
 pub async fn load_with_token(
     token: &str,
     executor: impl sqlx::Executor<'_, Database = Postgres>,
--- blueprint/db/src/lib.rs
+++ blueprint/db/src/lib.rs
@@ -5,18 +5,24 @@ use thiserror::Error;
 
 pub use sqlx::postgres::PgPool as DbPool;
 
+/// Entity definitions and related functions
 pub mod entities;
 
-#[doc(hidden)]
-pub async fn connect_pool(config: DatabaseConfig) -> Result<DbPool, anyhow::Error> {
-    let pool = PgPoolOptions::new()
-        .connect(config.url.as_str())
-        .await
-        .context("Failed to connect to database")?;
-
-    Ok(pool)
-}
-
+/// Starts a new database transaction.
+///
+/// Example:
+/// ```
+/// let tx = transaction(&app_state.db_pool).await?;
+/// tasks::create(task_data, &mut *tx)?;
+/// users::create(user_data, &mut *tx)?;
+/// 
+/// match tx.commit().await {
+///     Ok(_) => Ok((StatusCode::CREATED, Json(results))),
+///     Err(e) => Err((internal_error(e), "".into())),
+/// }
+/// ```
+///
+/// Transactions are rolled back automatically when they are dropped without having been committed.
 pub async fn transaction(
     db_pool: &DbPool,
 ) -> Result<Transaction<'static, Postgres>, anyhow::Error> {
@@ -28,15 +34,30 @@ pub async fn transaction(
     Ok(tx)
 }
 
+/// Errors that can occur as a result of a data layer operation.
 #[derive(Error, Debug)]
 pub enum Error {
+    /// General database error, e.g. communicating with the database failed
     #[error("database query failed")]
     DbError(anyhow::Error),
+    /// No record was found where one was expected, e.g. when loading a record by ID
     #[error("no record found where one was expected")]
     NoRecordFound,
     #[error("validation failed")]
+    /// An invalid changeset was passed to a writing operation such as creating or updating a record.
     ValidationError(validator::ValidationErrors),
 }
 
-#[cfg(feature = "test-helpers")]
+#[doc(hidden)]
+pub async fn connect_pool(config: DatabaseConfig) -> Result<DbPool, anyhow::Error> {
+    let pool = PgPoolOptions::new()
+        .connect(config.url.as_str())
+        .await
+        .context("Failed to connect to database")?;
+
+    Ok(pool)
+}
+
+#[cfg(any(feature = "test-helpers", doc))]
+#[doc(cfg(feature = "test-helpers"))]
 pub mod test_helpers;
