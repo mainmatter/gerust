44105fd0e82c3d2263422ff3de3dde005b881161
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-05-08 19:44:40 +0200

    WIP: docs for db crate

--- blueprint/db/src/entities/tasks.rs
+++ blueprint/db/src/entities/tasks.rs
@@ -6,20 +6,34 @@ use sqlx::Postgres;
 use uuid::Uuid;
 use validator::Validate;
 
+/// A task, i.e. TODO item.
 #[derive(Serialize, Debug, Deserialize)]
 pub struct Task {
+    /// The id of the record.
     pub id: Uuid,
+    /// The description, i.e. what to do.
     pub description: String,
 }
 
+/// A changeset representing the data that is intended to be used to either create a new task or update an existing task.
+///
+/// Changesets are validatated in the [`create`] and [`update`] functions which return an [Result::Err] if validation fails.
+///
+/// Changesets can also be used to generate fake data for tests when the `test-helpers` feature is enabled:
+///
+/// ```
+/// let task_changeset: TaskChangeset = Faker.fake();
+/// ```
 #[derive(Deserialize, Validate, Clone)]
 #[cfg_attr(feature = "test-helpers", derive(Serialize, Dummy))]
 pub struct TaskChangeset {
+    /// The description must be at least 1 character long.
     #[cfg_attr(feature = "test-helpers", dummy(faker = "Sentence(3..8)"))]
     #[validate(length(min = 1))]
     pub description: String,
 }
 
+/// Load all [`Task`]s from the database.
 pub async fn load_all(
     executor: impl sqlx::Executor<'_, Database = Postgres>,
 ) -> Result<Vec<Task>, anyhow::Error> {
@@ -29,6 +43,9 @@ pub async fn load_all(
     Ok(tasks)
 }
 
+/// Load one [`Task`] from the database identified by its ID.
+///
+/// If no record can be found for the ID, a [`crate::Error::NoRecordFound`] will be returned.
 pub async fn load(
     id: Uuid,
     executor: impl sqlx::Executor<'_, Database = Postgres>,
@@ -43,6 +60,9 @@ pub async fn load(
     }
 }
 
+/// Delete a [`Task`] from the database identified by its ID.
+///
+/// If no record can be found for the ID, a [`crate::Error::NoRecordFound`] will be returned.
 pub async fn delete(
     id: Uuid,
     executor: impl sqlx::Executor<'_, Database = Postgres>,
@@ -57,6 +77,9 @@ pub async fn delete(
     }
 }
 
+/// Create a task in the database with the data in the passed [`TaskChangeset`].
+///
+/// If the data in the changeset isn't valid, a [`crate::Error::ValidationError`] will be returned, otherwise the created task is returned.
 pub async fn create(
     task: TaskChangeset,
     executor: impl sqlx::Executor<'_, Database = Postgres>,
@@ -77,6 +100,9 @@ pub async fn create(
     })
 }
 
+/// Updates a task in the database with the data in the passed [`TaskChangeset`].
+///
+/// If the data in the changeset isn't valid, a [`crate::Error::ValidationError`] will be returned, otherwise the updated [`Task`] is returned. If no record can be found for the ID, a [`crate::Error::NoRecordFound`] will be returned.
 pub async fn update(
     id: Uuid,
     task: TaskChangeset,
