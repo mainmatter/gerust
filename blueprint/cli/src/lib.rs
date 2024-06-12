18ac565cded8b9086a667991ba0ff9972a6b94b2
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-06-12 12:22:32 +0200

    add docs for CLI crate

--- blueprint/cli/src/lib.rs
+++ blueprint/cli/src/lib.rs
@@ -1,1 +1,4 @@
+//! The {{project-name}}-cli crate implements the project's CLI tools `db` and `generate` as well as contains functionality for displaying information in a console UI.
+
+/// Utilities for CLIs
 pub mod util;
