04b2f3f6f3e78562aa6e2c690d362781bab59f78
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-06-30 18:28:22 +0200

    WIP: API docs for db crate

--- blueprint/db/src/test_helpers/users.rs
+++ blueprint/db/src/test_helpers/users.rs
@@ -2,14 +2,28 @@ use crate::entities::users::User;
 use fake::{faker::name::en::*, Dummy};
 use sqlx::postgres::PgPool;
 
+/// A changeset representing the data that is intended to be used to either create a new user or update an existing user.
+///
+/// Changesets are validatated in the [`create`] and [`update`] functions which return an [Result::Err] if validation fails.
+///
+/// Changesets can also be used to generate fake data for tests when the `test-helpers` feature is enabled:
+///
+/// ```
+/// let user_changeset: UserChangeset = Faker.fake();
+/// ```
 #[derive(Debug, Clone, Dummy)]
 pub struct UserChangeset {
+    /// The user's name
     #[dummy(faker = "Name()")]
     pub name: String,
+    /// The user's auth token, fake data will be a 100 characters long number
     #[dummy(faker = "100..101")]
     pub token: String,
 }
 
+/// Creates a user in the database with the data in the passed [`UserChangeset`].
+///
+/// If the data in the changeset isn't valid, a [`crate::Error::ValidationError`] will be returned, otherwise the created user is returned.
 pub async fn create(user: UserChangeset, db: &PgPool) -> Result<User, anyhow::Error> {
     let record = sqlx::query!(
         "INSERT INTO users (name, token) VALUES ($1, $2) RETURNING id",
