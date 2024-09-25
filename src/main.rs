c2e43c1567e864174863f6eff731e7f31d319790
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-09-25 13:25:06 +0200

    fix docs validation for pacesetter itself

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![deny(missing_docs)]
-
 //! Pacesetter provides blueprints and generators for axum projects. It establishes a standard project structure with a folder layout, standard patterns for composing applications into e.g. database access and web API, running tests and migrations, as well as tracing.
 
 use anyhow::Context;
