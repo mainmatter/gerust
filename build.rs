1953ad1152ee3706a9e42abd465338fc0e839def
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-10-13 13:45:28 +0200

    use vergen 9 APIs
    (and new packages)

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
 #![allow(missing_docs)]
 use std::error::Error;
 
-use vergen::EmitBuilder;
+use vergen::Emitter;
+use vergen_git2::Git2Builder;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    EmitBuilder::builder()
-        // Emit VERGEN_GIT_SHA
-        .git_sha(true)
+    Emitter::default()
+        .add_instructions(&Git2Builder::all_git()?)?
         .emit()?;
+
     println!("cargo:rerun-if-changed=blueprint");
     Ok(())
 }
