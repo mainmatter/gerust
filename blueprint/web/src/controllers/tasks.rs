bddbd861a03f775ad1ef54956aef42660195e746
Henk Oordt <hd@oordt.dev>
2024-10-23 16:39:48 +0200

    add macros for generating payload structs, use them in generated tasks controller

--- blueprint/web/src/controllers/tasks.rs
+++ blueprint/web/src/controllers/tasks.rs
@@ -1,6 +1,7 @@
 use crate::{error::Error, state::AppState};
 use axum::{extract::Path, extract::State, http::StatusCode, Json};
 use {{crate_name}}_db::{entities::tasks, transaction};
+use payloads::*;
 use tracing::info;
 use uuid::Uuid;
 
@@ -10,34 +11,34 @@ use uuid::Uuid;
 #[axum::debug_handler]
 pub async fn create(
     State(app_state): State<AppState>,
-    Json(task): Json<tasks::TaskChangeset>,
-) -> Result<(StatusCode, Json<tasks::Task>), Error> {
-    Ok(tasks::create(task, &app_state.db_pool)
+    Json(payload): Json<CreateRequestPayload>,
+) -> Result<(StatusCode, Json<CreateResponsePayload>), Error> {
+    Ok(tasks::create(payload.into(), &app_state.db_pool)
         .await
-        .map(|task| (StatusCode::CREATED, Json(task)))?)
+        .map(|task| (StatusCode::CREATED, Json(task.into())))?)
 }
 
 /// Creates multiple tasks in the database.
 ///
-/// This function creates multiple tasks in the database (see [`{{crate_name}}_db::entities::tasks::create`]) based on [`{{crate_name}}_db::entities::tasks::TaskChangeset`]s (sent as JSON). If all tasks are created successfully, a 201 response is returned with the created [`{{crate_name}}_db::entities::tasks::Task`]s' JSON representation in the response body. If any of the passed changesets is invalid, a 422 response is returned.
+/// This function creates multiple tasks in the database (see [`getest_db::entities::tasks::create`]) based on [`getest_db::entities::tasks::TaskChangeset`]s (sent as JSON). If all tasks are created successfully, a 201 response is returned with the created [`getest_db::entities::tasks::Task`]s' JSON representation in the response body. If any of the passed changesets is invalid, a 422 response is returned.
 ///
 /// This function creates all tasks in a transaction so that either all are created successfully or none is.
 #[axum::debug_handler]
 pub async fn create_batch(
     State(app_state): State<AppState>,
-    Json(tasks): Json<Vec<tasks::TaskChangeset>>,
-) -> Result<(StatusCode, Json<Vec<tasks::Task>>), Error> {
+    Json(payload): Json<CreateBatchRequestPayload>,
+) -> Result<(StatusCode, Json<CreateBatchResponsePayload>), Error> {
     let mut tx = transaction(&app_state.db_pool).await?;
 
     let mut results: Vec<tasks::Task> = vec![];
-    for task in tasks {
+    for task in Vec::<_>::from(payload) {
         let task = tasks::create(task, &mut *tx).await?;
         results.push(task);
     }
 
     tx.commit().await.map_err(anyhow::Error::from)?;
 
-    Ok((StatusCode::CREATED, Json(results)))
+    Ok((StatusCode::CREATED, Json(results.into())))
 }
 
 /// Reads and responds with all the tasks currently present in the database.
@@ -71,10 +72,10 @@ pub async fn read_one(
 pub async fn update(
     State(app_state): State<AppState>,
     Path(id): Path<Uuid>,
-    Json(task): Json<tasks::TaskChangeset>,
-) -> Result<Json<tasks::Task>, Error> {
-    let task = tasks::update(id, task, &app_state.db_pool).await?;
-    Ok(Json(task))
+    Json(payload): Json<UpdateRequestPayload>,
+) -> Result<Json<UpdateResponsePayload>, Error> {
+    let task = tasks::update(id, payload.into(), &app_state.db_pool).await?;
+    Ok(Json(task.into()))
 }
 
 /// Deletes a task identified by its ID from the database.
@@ -87,4 +88,33 @@ pub async fn delete(
 ) -> Result<StatusCode, Error> {
     tasks::delete(id, &app_state.db_pool).await?;
     Ok(StatusCode::NO_CONTENT)
+}
+
+mod payloads {
+    use {{crate_name}}_db::entities::tasks::{Task, TaskChangeset};
+    use {{crate_name}}_macros::{batch_request_payload, request_payload, response_payload};
+
+    #[derive(Debug)]
+    #[request_payload]
+    pub struct CreateRequestPayload(TaskChangeset);
+
+    #[derive(Debug)]
+    #[response_payload]
+    pub struct CreateResponsePayload(Task);
+
+    #[derive(Debug)]
+    #[batch_request_payload]
+    pub struct CreateBatchRequestPayload(Vec<TaskChangeset>);
+
+    #[derive(Debug)]
+    #[response_payload]
+    pub struct CreateBatchResponsePayload(Vec<Task>);
+
+    #[derive(Debug)]
+    #[request_payload]
+    pub struct UpdateRequestPayload(TaskChangeset);
+
+    #[derive(Debug)]
+    #[response_payload]
+    pub struct UpdateResponsePayload(Task);
 }
