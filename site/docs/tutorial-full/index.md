bedd57fafc381195a671f2536aa5f9fbfe17c72d
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-11-02 17:39:16 +0100

    WIP: content outline

--- site/docs/tutorial-full/index.md
+++ site/docs/tutorial-full/index.md
@@ -1,5 +1,1 @@
----
-sidebar_position: 4
----
-
 # Full apps
--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -17,10 +17,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/"
-          >
+          <Link className="button button--secondary button--lg" to="/docs/">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
