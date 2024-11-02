780c181e6d6dcfdf7ffd2de6e194a254256ce622
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-11-02 15:54:46 +0100

    add basic Docusaurus page

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,99 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
+
+  url: "https://docs.gerust.rs",
+  baseUrl: "/",
+
+  organizationName: "mainmatter",
+  projectName: "gerust",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/mainmatter/gerust/tree/main/docs/",
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Gerust Docs",
+      logo: {
+        alt: "Gerust Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
+        },
+        {
+          href: "https://github.com/mainmatter/gerust/",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              href: "https://mainmatter.com/rust/",
+              label: "Mainmatter",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/mainmatter/gerust",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Mainmatter GmbH and contributors`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
