6b43935392c4dc245d20f0245d65169975e48bc5
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-11-25 11:46:49 +0100

    add landing page

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,12 +1,11 @@
 import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import ThemedImage from "@theme/ThemedImage";
 import styles from "./index.module.css";
-import LogoImage from '@site/static/img/logo.svg';
-import LogoImageDark from '@site/static/img/logo-dark-mode.svg';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -16,20 +15,37 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <div className="hero__logo">
-          <LogoImage className="hero__logo-light" />
-          <LogoImageDark className="hero__logo-dark" />
-        </div>
+        <ThemedImage
+          alt="Gerust logo"
+          sources={{
+            light: useBaseUrl("/img/logo.svg"),
+            dark: useBaseUrl("/img/logo-dark-mode.svg"),
+          }}
+        />
+        <p className={styles.heroText}>
+          Gerust takes care of the accidental complexity of Rust backend
+          projects so you can stay focused on what matters.
+        </p>
+        <p className={styles.heroText}>
+          Gerust projects build on top of{" "}
+          <a href="https://github.com/tokio-rs/axum" target="_blank">
+            axum
+          </a>{" "}
+          and{" "}
+          <a href="https://github.com/launchbadge/sqlx" target="_blank">
+            SQLx
+          </a>{" "}
+          – proven crates that are widely used in the Rust ecosystem.
+        </p>
       </div>
     </header>
   );
 }
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Gerust: Rust backend project generator and manager`}
+      title="Gerust: Rust backend project generator and manager"
       description="Gerust is a project generator and manager for Rust backend projects. It provides an architecture and tooling so you can stay focused on what matters."
     >
       <HomepageHeader />
