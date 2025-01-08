be15140c80466fc91e34c0cda99541268b169ec1
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2025-01-08 14:37:11 +0100

    return ExitCode from main binary as well

--- blueprint/web/src/main.rs
+++ blueprint/web/src/main.rs
@@ -1,15 +1,20 @@
 #![allow(missing_docs)]
 use {{crate_name}}_web::{init_tracing, run};
+use std::process::ExitCode;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ExitCode {
     init_tracing();
 
-    if let Err(e) = run().await {
-        tracing::error!(
-            error.msg = %e,
-            error.error_chain = ?e,
-            "Shutting down due to error"
-        )
+    match run().await {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(e) => {
+            tracing::error!(
+                error.msg = %e,
+                error.error_chain = ?e,
+                "Shutting down due to error"
+            );
+            ExitCode::FAILURE
+        }
     }
 }
